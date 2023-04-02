11a55345f7ba528ddeb2c6909444374a0558b47f
maksym2493 <124437670+maksym2493@users.noreply.github.com>
2023-04-02 12:49:20 +0300

    Update StartupOnReboot.kt

--- app/src/main/java/com/example/stepcounterwef/StartupOnReboot.kt
+++ app/src/main/java/com/example/stepcounterwef/StartupOnReboot.kt
@@ -7,7 +7,9 @@ import androidx.core.content.ContextCompat
 
 class StartupOnReboot: BroadcastReceiver(){
     override fun onReceive(context: Context, intent: Intent) {
-        Data.init(context.filesDir)
-        ContextCompat.startForegroundService(context, Intent(context, StepCounter::class.java))
+        if(ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED){
+            Data.init(context.filesDir)
+            ContextCompat.startForegroundService(context, Intent(context, StepCounter::class.java))
+        }
     }
 }
